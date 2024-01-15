b44d441c343208181fbcff3d2c6efb1b97f2ed89
alokie <alokie@pop-os.localdomain>
2024-01-15 15:57:32 +0530

    added timeline,plantInfo & other changes

--- src/App.js
+++ src/App.js
@@ -240,6 +240,7 @@ function App() {
                           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                       </button>
+                      <div></div>
                      
                     </div>
                   </div>
